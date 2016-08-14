d8b7cd0a3c81c534cf4de4c6e02283b61035756a
Zach Ahn <zach.ahn@gmail.com>
2016-08-13 22:50:42 -0400

    Add tests demonstrating what works and doesn't

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,4 +6,22 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should be able to log in manually" do
+    user = FactoryGirl.create(:user, email: "test@example.com", password: "letmein")
+
+    post session_url, params: { session: { email: "test@example.com", password: "letmein" } }
+
+    get root_url
+
+    assert_select("p", "test@example.com")
+  end
+
+  test "should be able to log in using `#sign_in_as`" do
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+  end
+
+  test "should be able to log in using `#sign_in`" do
+    sign_in
+  end
 end
---
+++ test/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email "example@example.com"
+    password "password1"
+  end
+end
